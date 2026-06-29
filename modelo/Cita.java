0f3e384f2df22315febf671b1721a80d2cea2595
andre <riveraquinoandrea@gmail.com>
2026-06-29 14:27:44 -0500

    Add files via upload
    cita.java

---
+++ modelo/Cita.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelcliente;
+
+/**
+ *
+ * @author HOME
+ */
+public class Cita {
+     private int id;
+    private String nombre;
+    private String telefono;
+    private String servicio;
+    private String fecha;
+    private String hora;
+
+    
+    public Cita() {
+
+    }
+
+  
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getServicio() {
+        return servicio;
+    }
+
+    public void setServicio(String servicio) {
+        this.servicio = servicio;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+}
+
